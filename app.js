bfae261a078e2eafd69066f700ccb956b346d542
Marlon Villalona <iamvillalona@gmail.com>
2024-09-21 22:23:22 -0400

    npm init and package.json

--- app.js
+++
@@ -1,8 +0,0 @@
-// ************* Manipular archivos  ************
-const fs = require("fs"); // File System
-
-const file = fs.readFileSync("newfile.txt", "utf-8"); // Lee el archivo
-
-const fileMofidied = file.replace(/Node.js/ig, 'NodeJS'); // Reemplaza el texto
-
-fs.writeFileSync("newfilemodified2.txt", fileMofidied); // Escribe el archivo

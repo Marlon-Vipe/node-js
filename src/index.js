1d2cbf0483b2d980c679a4fabdb2eaa81c08fd3c
Marlon Villalona <iamvillalona@gmail.com>
2024-09-22 01:16:10 -0400

    .env configuration

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 // const { system } = require('./objects')// Importamos el objeto
 //const { getTitle, getAuthor } = require('./functions')// Importamos las funciones
 
-import { system } from './objects.js'
-import { getTitle, getAuthor } from './functions.js'
+//import { system } from './objects.js'
+//import { getTitle, getAuthor } from './functions.js'
+import { config } from 'dotenv'; // Importamos la funcion config de dotenv
+import env from 'env-var' // Importamos la libreria env-var
 
 // Mostramos el sistema
-console.log(`${system.nombre} - ${getTitle()} - ${getAuthor(system.author)}`)
+
+//console.log(`${system.nombre} - ${getTitle()} - ${getAuthor(system.author)}`)
+config() // Cargamos las variables de entorno
+
+const PORT = env.get('PORT').asPortNumber() // Obtenemos la variable de entorno PORT
+
+console.log(process.env.PORT) // Mostramos la variable de entorno PORT
